b6279ec53408a2cdced81dd7d89e601ac8f6e762
Irene Ye Yuan <ireneyeyuan@gmail.com>
2021-09-13 13:45:32 -0500

    Update generics example

---
+++ src/LectureExamples/GenericExamples.java
@@ -0,0 +1,28 @@
+package LectureExamples;
+
+public class GenericExamples {
+    public static void main(String[] args){
+        // instance of Integer type
+        Test <Integer> intObjects = new Test<Integer>(15);
+        System.out.println(intObjects.getObject());
+   
+        // instance of String type
+        Test <String> strObjects = new Test<String>("TestingStrings");
+        System.out.println(strObjects.getObject());
+
+
+    }
+}
+
+class Test<T> {
+    // An object of type T is declared
+    T object;
+    // constructor
+    Test(T object) {
+          this.object = object;  
+    }  
+    public T getObject() {
+        return this.object; 
+    }
+}
+ 

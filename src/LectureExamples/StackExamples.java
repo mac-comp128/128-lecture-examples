2bff818db6a126983fc39d7b6e1c34e8c09d389b
Irene Y. Yuan <yuan0191@umn.edu>
2022-01-30 23:04:57 -0600

    Update examples for stacks

---
+++ src/LectureExamples/StackExamples.java
@@ -0,0 +1,41 @@
+package LectureExamples;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StackExamples {
+
+    public static void main(String[] args){
+        
+        // Initializing a stack in Java using deque class with arraydeque implementation
+        Deque<String> stackOfStrings = new ArrayDeque<String>();
+        // Initializing a stack in Java using deque class with linked list implementation
+        //Deque<String> stackOfStrings = new LinkedList<String>();
+
+        // Add an element to the stack using push method
+        stackOfStrings.push("apple");
+
+        // Take a look at the top of the stack using peek method
+        System.out.println("The current top element is: "+stackOfStrings.peek());
+
+        // Push two more elements to the top of the stack
+        stackOfStrings.push("pear");
+        stackOfStrings.push("kiwi");
+
+        // Now the top of the stack will be the last element added 
+        System.out.println("The current top element is: "+stackOfStrings.peek());
+        System.out.println(" ");
+
+        // An example of iterating through a stack
+        while(!stackOfStrings.isEmpty()){
+            System.out.println("Element popped: "+stackOfStrings.pop());
+        }
+
+        // Check the size of the stack after popping all elements
+        System.out.println(" ");
+        System.out.println("The current size of the stack: "+stackOfStrings.size());
+        
+
+    }
+}

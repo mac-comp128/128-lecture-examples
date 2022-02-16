dc3cb1963b9de302c13ab78ac1a035468c978044
Irene Y. Yuan <yuan0191@umn.edu>
2022-02-16 07:43:47 -0600

    Update list examples

---
+++ src/LectureExamples/ListExamples.java
@@ -0,0 +1,68 @@
+public class ListExamples {
+    public static void main(String[] args){
+        // make some new student instances
+        Student sam = new Student("Gilbert", "Sam", 2220390);  Student john = new Student("Smith", "John", 333333);
+        Student libby = new Student("Shoop", "Libby", 4444444);
+
+        List<Student> studentList = new ArrayList<Student>();  
+
+        studentList.add(sam);
+        studentList.add(john);
+        studentList.add(libby);
+
+        // 1. While a familiar approach, this for loop example
+        // is not typically what we use to access every element 
+        // in a List
+
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
+
+        // // 2. This style is preferred for both efficiency and
+        // // elegance of the code itself.
+        // // It is ideal for traversal of all elements  or some 
+        // // elements until a given one is found
+
+        // for (Student stu : studentList) {  
+        //     System.out.println(stu);
+        // }
+
+        // // 2a. Using the colon style and repeat until sentinel
+        // // to find a desired element.
+
+        // for (Student stu : studentList) {  
+        //     if (stu.getID() == 333333) {  
+        //         System.out.println(stu);
+        //         break;
+        //     }
+        // }
+    
+        // // 2b. Using stream
+        // studentList.stream().forEach(System.out::println);
+
+
+        // // 3. Using an Iterator.
+        // // Must include java.util.Iterator
+
+        // Iterator iter = studentList.iterator();  
+        // while (iter.hasNext()) {
+        //     Student stu = (Student) iter.next();  System.out.println(stu);
+        // }
+
+        // // 3a. Using an Iterator.
+        // // This enables us to change the list while iterating over it.
+
+        // Iterator iter2 = studentList.iterator();  
+        // while (iter2.hasNext()) {
+        //     Student stu = (Student) iter2.next();  
+        //     if (stu.getID() == 333333) {
+        //         //	note that using remove on Iterator object is a
+        //         //	safe way to remove the element and keep going
+        //         iter2.remove();
+        //     }
+        // }
+        // System.out.println("List after removing one student:");  
+        // System.out.println(studentList);
+
+    }
+}
---
+++ src/LectureExamples/Student.java
@@ -0,0 +1,81 @@
+public class Student {
+    /* Private instance variables */
+    private String studentName;   /* The student's name           */
+    private int studentID;        /* The student's ID number      */
+    private double creditsEarned; /* The number of credits earned */
+    private boolean paidUp;       /* Whether student is paid up   */
+
+	/* Public constants */
+
+    /** The number of credits required for graduation */
+    public static final double CREDITS_TO_GRADUATE = 128.0;
+
+    /**
+     * Creates a new Student object with the specified name and ID.
+     * @param name The student's name as a String
+     * @param id The student's ID number as an int
+     */
+    public Student(String name, int id) {
+        studentName = name;
+        studentID = id;
+    }
+
+    /**
+     * Gets the name of this student.
+     * @return The name of this student
+     */
+    public String getName() {
+        return studentName;
+    }
+
+    /**
+     * Gets the ID number of this student.
+     * @return The ID number of this student
+     */
+    public int getID() {
+        return studentID;
+    }
+
+    /**
+     * Sets the number of credits earned.
+     * @param credits The new number of credits earned
+     */
+    public void setCredits(double credits) {
+        creditsEarned = credits;
+    }
+
+    /**
+     * Gets the number of credits earned.
+     * @return The number of credits this student has earned
+     */
+    public double getCredits() {
+        return creditsEarned;
+    }
+
+    /**
+     * Sets whether the student is paid up.
+     * @param flag The value true or false indicating paid-up status
+     */
+    public void setPaidUp(boolean flag) {
+        paidUp = flag;
+    }
+
+    /**
+     * Returns whether the student is paid up.
+     * @return Whether the student is paid up
+     */
+    public boolean isPaidUp() {
+        return paidUp;
+    }
+
+    /**
+     * Creates a string identifying this student.
+     * @return The string used to display this student
+     */
+    @Override
+    public String toString() {
+        return studentName + " (#" + studentID + ")";
+    }
+
+   
+}
